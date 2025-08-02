684e014d01d5165c4754ecaf339a01798eafd575
priya <priya211@gmail.com>
2025-08-02 23:47:13 +0530

    search functionality

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,22 @@ import { CoinContext } from '../../context/CoinContext';
 const Home = () => {
     const{allCoin,currency}=useContext(CoinContext);
     const[displayCoin,setDisplayCoin]=useState([]);
+    const[input,setInput]=useState("");
+    const inputHandler=(event)=>{
+        setInput(event.target.value);
+        if(event.target.value===""){
+            setDisplayCoin(allCoin);
+        }
+    }
+    const searchHandler=async (event)=>{
+        event.preventDefault();
+        const coins=await allCoin.filter((item)=>{
+           return item.name.toLowerCase().includes(input.toLowerCase()) 
+    })
+    //bitcoin
+    //bit
+    setDisplayCoin(coins);
+}
     useEffect(()=>{
         setDisplayCoin(allCoin)},[allCoin]);
     
@@ -14,8 +30,11 @@ const Home = () => {
             <p>Welcome to the world's largest cryptocurrency
                 marketplace.Sign up to explore more about cryptos.
             </p>
-            <form>
-                <input type="text" placeholder='Search crypto..'/>
+            <form onSubmit={searchHandler}>
+                <input onChange={inputHandler}  list='coinlist'value={input} type="text" placeholder='Search crypto..' required/>
+                <datalist id="coinlist">
+                    {allCoin.map((item,index)=>(<option key={index} value={item.name}/>))}
+                </datalist>
                 <button>Search</button>
             </form>
         </div>
@@ -31,6 +50,16 @@ const Home = () => {
                 displayCoin.slice(0,10).map((item,index)=>(
                     <div className="table-layout" key={index}>
                         <p>{item.market_cap_rank}</p>
+                        <div>
+                            <img src={item.image} alt=""></img>
+                            <p>{item.name+" - " +item.symbol}</p>
+                           
+                        </div>
+                         <p>{currency.symbol}{item.current_price.toLocaleString()}</p>
+                         <p className={item.price_change_percentage_24h<0?"red":"green"}>
+                            {Math.floor(item.price_change_percentage_24h*100)/100}</p>
+                         <p className='market-cap'>{currency.symbol}{item.market_cap.toLocaleString()}</p>
+
                     </div>
                 ))
             }
